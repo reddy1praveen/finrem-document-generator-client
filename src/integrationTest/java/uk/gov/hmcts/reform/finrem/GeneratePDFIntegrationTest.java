055634ab09b7a57fc4c31132cf8a02d2edfd71b7
shaibazkhan <sknizami@gmail.com>
2018-10-26 02:21:19 +0100

    integration test support classes

--- src/integrationTest/java/uk/gov/hmcts/reform/finrem/GeneratePDFIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/finrem/GeneratePDFIntegrationTest.java
@@ -2,8 +2,16 @@ package uk.gov.hmcts.reform.finrem;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
 
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {ServiceContextConfiguration.class})
+@EnableFeignClients(basePackageClasses = ServiceAuthorisationApi.class)
 public class GeneratePDFIntegrationTest {
 
     @Test
---
+++ src/integrationTest/java/uk/gov/hmcts/reform/finrem/IdamUtils.java
@@ -0,0 +1,120 @@
+package uk.gov.hmcts.reform.finrem;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.springframework.beans.factory.annotation.Value;
+import uk.gov.hmcts.reform.finrem.model.CreateUserRequest;
+import uk.gov.hmcts.reform.finrem.model.UserCode;
+
+import java.util.Base64;
+import java.util.Locale;
+import java.util.UUID;
+
+public class IdamUtils {
+
+    @Value("${auth.idam.client.baseUrl}")
+    private String idamUserBaseUrl;
+
+    @Value("${auth.idam.client.redirectUri}")
+    private String idamRedirectUri;
+
+    @Value("${auth.idam.client.secret}")
+    private String idamSecret;
+
+    private String idamUsername;
+
+    private String idamPassword;
+
+    private String testUserJwtToken;
+
+    private String testCaseworkerJwtToken;
+
+    public String generateNewUserAndReturnToken() {
+        String username = "simulate-delivered" + UUID.randomUUID() + "@notifications.service.gov.uk";
+        String password = UUID.randomUUID().toString().toUpperCase(Locale.UK);
+        createUserInIdam(username, password);
+        return generateUserTokenWithNoRoles(username, password);
+    }
+
+    private void createUserInIdam(String username, String password) {
+        CreateUserRequest userRequest = CreateUserRequest.builder()
+            .email(username)
+            .password(password)
+            .forename("Test")
+            .surname("User")
+            .roles(new UserCode[] { UserCode.builder().code("citizen").build() })
+            .userGroup(UserCode.builder().code("finrem-private-beta").build())
+            .build();
+
+        RestAssured.given()
+            .header("Content-Type", "application/json")
+            .body(ResourceLoader.objectToJson(userRequest))
+            .post(idamCreateUrl());
+    }
+
+    private void createUserInIdam() {
+        idamUsername = "simulate-delivered" + UUID.randomUUID() + "@notifications.service.gov.uk";
+        idamPassword = UUID.randomUUID().toString();
+
+        createUserInIdam(idamUsername, idamPassword);
+    }
+
+    public void createCaseworkerUserInIdam(String username, String password) {
+        CreateUserRequest userRequest = CreateUserRequest.builder()
+            .email(username)
+            .password(password)
+            .forename("Test")
+            .surname("User")
+            .roles(new UserCode[] { UserCode.builder().code("caseworker-divorce-courtadmin").build() })
+            .userGroup(UserCode.builder().code("caseworker").build())
+            .build();
+
+        RestAssured.given()
+            .header("Content-Type", "application/json")
+            .body(ResourceLoader.objectToJson(userRequest))
+            .post(idamCreateUrl());
+    }
+
+    private String idamCreateUrl() {
+        return idamUserBaseUrl + "/testing-support/accounts";
+    }
+
+    public String generateUserTokenWithNoRoles(String username, String password) {
+        String userLoginDetails = String.join(":", username, password);
+        final String authHeader = "Basic " + new String(Base64.getEncoder().encode((userLoginDetails).getBytes()));
+
+        Response response = RestAssured.given()
+            .header("Authorization", authHeader)
+            .relaxedHTTPSValidation()
+            .post(idamCodeUrl());
+
+        if (response.getStatusCode() >= 300) {
+            throw new IllegalStateException("Token generation failed with code: " + response.getStatusCode()
+                + " body: " + response.getBody().prettyPrint());
+        }
+
+        response = RestAssured.given()
+            .relaxedHTTPSValidation()
+            .post(idamTokenUrl(response.getBody().path("code")));
+
+        String token = response.getBody().path("access_token");
+        return "Bearer " + token;
+    }
+
+    private String idamCodeUrl() {
+        return idamUserBaseUrl + "/oauth2/authorize"
+            + "?response_type=code"
+            + "&client_id=divorce"
+            + "&redirect_uri=" + idamRedirectUri;
+    }
+
+    private String idamTokenUrl(String code) {
+
+        return idamUserBaseUrl + "/oauth2/token"
+            + "?code=" + code
+            + "&client_id=divorce"
+            + "&client_secret=" + idamSecret
+            + "&redirect_uri=" + idamRedirectUri
+            + "&grant_type=authorization_code";
+    }
+}
