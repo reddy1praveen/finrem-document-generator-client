e2a2e805525adabf25cc0c6d769e7f433c54d8eb
shaibazkhan <sknizami@gmail.com>
2019-01-04 21:37:29 +0000

    refactored merging doc code into utility class

--- src/main/java/uk/gov/hmcts/reform/finrem/documentgenerator/service/DocumentOverlayService.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/documentgenerator/service/DocumentOverlayService.java
@@ -1,23 +1,16 @@
 package uk.gov.hmcts.reform.finrem.documentgenerator.service;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.pdfbox.cos.COSArray;
-import org.apache.pdfbox.cos.COSBase;
-import org.apache.pdfbox.cos.COSDictionary;
-import org.apache.pdfbox.cos.COSName;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-import uk.gov.hmcts.reform.finrem.documentgenerator.error.PDFGenerationException;
 import uk.gov.hmcts.reform.finrem.documentgenerator.model.Document;
 import uk.gov.hmcts.reform.finrem.documentgenerator.model.FileUploadResponse;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+
+import static java.util.concurrent.CompletableFuture.supplyAsync;
 
 @Service
 @Slf4j
@@ -32,41 +25,13 @@ public class DocumentOverlayService {
     @Async
     public CompletableFuture<Document> generateConsentOrderOverlay(String template, Map<String, Object> values,
                                                 String authorizationToken) {
-        CompletableFuture<byte[]> coverPage = CompletableFuture.supplyAsync(() -> coverPageData(template, values));
-        CompletableFuture<byte[]> consentOrder = CompletableFuture.supplyAsync(() -> consentOrderData(values, authorizationToken));
+        CompletableFuture<byte[]> coverPage = supplyAsync(() -> coverPageData(template, values));
+        CompletableFuture<byte[]> consentOrder = supplyAsync(() -> consentOrderData(values, authorizationToken));
 
         return coverPage
-            .thenCombine(consentOrder, this::merge)
+            .thenCombine(consentOrder, DocumentSupport::addCoverPage)
             .thenApply(overlayDoc -> storeDocument(overlayDoc, authorizationToken))
             .thenApply(DocumentSupport::convert);
-    }
-
-    private byte[] merge(byte[] coverPageData, byte[] consentOrderData) {
-        try {
-            PDDocument doc = PDDocument.load(consentOrderData);
-
-            PDDocument coverDoc = PDDocument.load(coverPageData);
-            PDPage coverPage = coverDoc.getPage(0);
-            doc.importPage(coverPage);
-
-
-            doc.importPage(coverPage);
-            COSDictionary pages = (COSDictionary) doc.getDocumentCatalog().getCOSObject().getDictionaryObject(COSName.PAGES);
-            COSArray kids = (COSArray) pages.getDictionaryObject(COSName.KIDS);
-
-            COSBase last = kids.get(kids.size() - 1);
-            kids.remove(last);
-            kids.add(0, last);
-
-            ByteArrayOutputStream bos = new ByteArrayOutputStream();
-
-            doc.save(bos);
-            coverDoc.close();
-
-            return bos.toByteArray();
-        } catch (IOException e) {
-            throw new PDFGenerationException("couldn't merge documents", e);
-        }
     }
 
     private FileUploadResponse storeDocument(byte[] document, String authorizationToken) {
--- src/main/java/uk/gov/hmcts/reform/finrem/documentgenerator/service/DocumentSupport.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/documentgenerator/service/DocumentSupport.java
@@ -1,7 +1,17 @@
 package uk.gov.hmcts.reform.finrem.documentgenerator.service;
 
+import org.apache.pdfbox.cos.COSArray;
+import org.apache.pdfbox.cos.COSBase;
+import org.apache.pdfbox.cos.COSDictionary;
+import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import uk.gov.hmcts.reform.finrem.documentgenerator.error.PDFGenerationException;
 import uk.gov.hmcts.reform.finrem.documentgenerator.model.Document;
 import uk.gov.hmcts.reform.finrem.documentgenerator.model.FileUploadResponse;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 import static java.lang.String.format;
 
@@ -17,7 +27,36 @@ public class DocumentSupport {
             .build();
     }
 
+    public static byte[] addCoverPage(byte[] coverPageData, byte[] mainDocData) {
+        try {
+            PDDocument doc = PDDocument.load(mainDocData);
+
+            PDDocument coverDoc = PDDocument.load(coverPageData);
+            PDPage coverPage = coverDoc.getPage(0);
+            doc.importPage(coverPage);
+
+            doc.importPage(coverPage);
+            COSDictionary pages = (COSDictionary) doc.getDocumentCatalog().getCOSObject().getDictionaryObject(COSName.PAGES);
+            COSArray kids = (COSArray) pages.getDictionaryObject(COSName.KIDS);
+
+            COSBase last = kids.get(kids.size() - 1);
+            kids.remove(last);
+            kids.add(0, last);
+
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+            doc.save(bos);
+            coverDoc.close();
+
+            return bos.toByteArray();
+        } catch (IOException e) {
+            throw new PDFGenerationException("couldn't merge documents", e);
+        }
+    }
+
     private static String toBinaryUrl(FileUploadResponse response) {
         return format("%s/binary", response.getFileUrl());
     }
+
+
 }
