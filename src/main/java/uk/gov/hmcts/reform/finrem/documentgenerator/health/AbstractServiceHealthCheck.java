a4147eb50dadf9972d0e8b981ba6b44128db5be1
shaibazkhan <sknizami@gmail.com>
2019-01-21 17:18:33 +0000

    removed unused import

--- src/main/java/uk/gov/hmcts/reform/finrem/documentgenerator/health/AbstractServiceHealthCheck.java
+++ src/main/java/uk/gov/hmcts/reform/finrem/documentgenerator/health/AbstractServiceHealthCheck.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.finrem.documentgenerator.health;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.http.HttpStatus;
