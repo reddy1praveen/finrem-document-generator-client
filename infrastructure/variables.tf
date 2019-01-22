75710f146f3f6d16bde9bd464f55ea1d0de8a36a
shaibazkhan <sknizami@gmail.com>
2019-01-22 12:13:53 +0000

    removed reference to unused vars

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -30,8 +30,6 @@ variable "client_id" {
   description = "(Required) The object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies. This is usually sourced from environment variables and not normally required to be specified."
 }
 
-variable "tenant_id" {}
-
 variable "jenkins_AAD_objectId" {
   type        = "string"
   description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
@@ -40,22 +38,6 @@ variable "jenkins_AAD_objectId" {
 variable "appinsights_instrumentation_key" {
     description = "Instrumentation key of the App Insights instance this webapp should use. Module will create own App Insights resource if this is not provided"
     default = ""
-}
-
-variable "idam_s2s_url_prefix" {
-  default = "rpe-service-auth-provider"
-}
-
-variable "auth_provider_service_client_microservice" {
-  default = "finrem_document_generator"
-}
-
-variable "auth_provider_service_client_tokentimetoliveinseconds" {
-  default = "900"
-}
-
-variable "pdf_service_url_part" {
-  default = "cmc-pdf-service"
 }
 
 variable "evidence_management_client_api_url_part" {
